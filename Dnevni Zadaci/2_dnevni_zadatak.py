85d243191bf6883c05232c1f74c4089f8f37fcde
d4nin <daninsadzak@gmail.com>
2025-10-22 15:59:10 +0200

    Dnevni Zadaci - Zadatak 2

---
+++ Dnevni Zadaci/2_dnevni_zadatak.py
@@ -0,0 +1,20 @@
+# Napisati kod koji ispisuje vase ime i prezime, datum rodjenja, zatim smjer, odsjek, fakultet, te jednu duzu
+# liniju sastavljenu od minusa. Svaki podatak treba da bude ispisan u novom redu. Ispod ovih podataka student
+# treba od zvjezdica da ispise svoje ime. Primjer moguceg ispisa navedem na PDF fajlu dnevnog zadatka:
+# https://pmfunsaba.sharepoint.com/sites/KN100SIT100-Programiranje12526/Zajednicki%20dokumenti/Dnevni%20zadaci/Dnevni%20zadatak%202.pdf
+
+print("Danin Sadzak")
+print("08.02.2006.")
+print("Kompjuterske Nauke")
+print("Odjsek za matematiku i kompjuterske nauke")
+print("Prirodno-Matematicki fakultet")
+print('-'*50) # Moze i da se rucno ispise kao "-----" itd. 
+
+# Ovdje ide vase ime, ali za primjer ovdje je "PMF". 
+print("********   **           **   *********")
+print("*      *   * *         * *   *        ")
+print("*      *   *  *       *  *   *        ")
+print("********   *   *     *   *   *********")
+print("*          *    *   *    *   *        ")
+print("*          *     * *     *   *        ")
+print("*          *      *      *   *        ")
