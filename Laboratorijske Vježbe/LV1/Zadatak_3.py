d12bc7418dc2ef77b67930093a1fb061b5707119
d4nin <daninsadzak@gmail.com>
2025-10-30 21:31:41 +0100

    Laboratorijske Vjezbe - LV 1 - Zadatak 3

---
+++ Laboratorijske Vježbe/LV1/Zadatak_3.py
@@ -0,0 +1,8 @@
+# Napisati program koji ispisuje u konzolu oblik koji predstavlja zastavu Bosne i Hercegovine.
+print(f"{'*-#######':->17}--")
+print(f"{'*-######':->17}--")
+print(f"{'*-#####':->17}--")
+print(f"{'*-####':->17}--")
+print(f"{'*-###':->17}--")
+print(f"{'*-##':->17}--")
+print(f"{'*-#':->17}--")
