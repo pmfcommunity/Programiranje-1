cb28ab050cecea17bdaf06c17e1fb5b4c6b14232
d4nin <daninsadzak@gmail.com>
2025-10-30 21:44:46 +0100

    Laboratorijske Vjezbe - LV1 - Zadatak 6

---
+++ Laboratorijske Vježbe/LV1/Zadatak_6.py
@@ -0,0 +1,5 @@
+# Napisati zadatak koji racuna vrijednost izraza:
+# https://pmfunsaba.sharepoint.com/sites/KN100SIT100-Programiranje12526/Zajednicki%20dokumenti/Vjezbe/Laboratorijske%20vje%C5%BEbe/KN/LV1/LV%201%20-%20Unos,%20Ispis,%20Varijable.pdf?CT=1761856935255&OR=ItemsView&wdOrigin=TEAMSFILE.FILEBROWSER.DOCUMENTLIBRARY
+
+rezultat = 1 / (1 + (1 / 2 + (1 / 3 + (1 / 4 + (1 / 5 + (1 / 6))))))
+print(f"Rezultat izraza je: {rezultat}")
