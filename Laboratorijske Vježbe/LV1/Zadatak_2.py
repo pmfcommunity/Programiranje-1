3d2da380d1062f11a82c8415a90c59ceb1c570e1
d4nin <daninsadzak@gmail.com>
2025-10-30 21:27:50 +0100

    Laboratorijske Vjezbe - LV1 - Zadatak 2

---
+++ Laboratorijske Vježbe/LV1/Zadatak_2.py
@@ -0,0 +1,8 @@
+# Napisati program koji ispisuje u konzolu tekst:
+# "Drugi zadatak je"
+# "vec malo komplikovaniji,"
+# "jer je napisan u vise redova, te koristi i '' i ""."
+
+print("Drugi zadatak je")
+print("Vec malo komplikovaniji,")
+print("jer je napisan u vise redova, te koristi i '' i \"\"")
