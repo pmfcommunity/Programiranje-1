118e5bca69b7be908f555222b0d871391bac3c82
d4nin <daninsadzak@gmail.com>
2025-12-06 22:20:47 +0100

    Auditorne Vjezbe - AV9 - Zadatak 1

---
+++ Auditorne Vježbe/AV9/Zadatak_1.py
@@ -0,0 +1,8 @@
+# Napisati funkciju koja prihvata dva realna broja kao parametre. Funkcija vraca veci od njih. 
+
+def veci(x, y):
+    return max(a, b)
+
+a = int(input())
+b = int(input())
+print(veci(a, b))
