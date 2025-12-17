0ffe2a7861d774eb403f58fb7432c4a5a4028595
d4nin <daninsadzak@gmail.com>
2025-12-17 07:14:00 +0100

    Laboratorijske Vjezbe - LV6 - Zadatak 2

---
+++ Laboratorijske Vježbe/LV6/Zadatak_2.py
@@ -0,0 +1,23 @@
+# Napisati program koji trazi unos prirodnog broja n i cijelih brojeva a, b. Program pravi listu od n elemenata popunjen slucajno 
+# generisanim cijelim brojevima u rasponu a do b. Na kraju se ispisuje lista, sa elementima razdvojenim zarezom. Napravite sljedece
+# varijante: 
+# - Brojevi u nizu se smiju ponavljati
+# - Brojevi u nizu se ne smiju ponavljati 
+
+import random
+
+n = int(input())
+a = int(input())
+b = int(input())
+
+lista_brojeva = []
+for _ in range(n):
+    lista_brojeva.append(random.randint(a, b))
+
+for broj in lista_brojeva: 
+    print(broj, end=" ")
+print()
+
+for broj in set(lista_brojeva):
+    print(broj, end=" ")
+print()
